67f7cb252e902a8d98af74f79c27c750b9a2eb96
smohammad <smohammad@ad.deepedge.ai>
2024-02-14 21:40:55 +0530

    folder structure added

--- template.py
+++ template.py
@@ -2,10 +2,10 @@ import os
 from pathlib import Path
 import logging
 
-
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
 
-project_name = "mlproject"
+
+project_name = "mlProject"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
@@ -28,20 +28,27 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
+    "templates/index.html",
+    "test.py"
 ]
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir,filename = os.path.split(filepath)
 
-    if filedir != "":
+    if filedir!="":
         os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+        logging.info(f"Creating directory; {filedir} for the file:{filename}")
+    
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,"w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
     else:
         logging.info(f"{filename} is already exists")
+
         
+
+
+
+
